41c0644c950b68dc8eef34bbf67397a4efafddf1
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-08 22:56:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,11 @@ def loop_message_five_times(message)
     count += 1 
   end 
 end 
+
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do 
+    put message 
+    count += 1 
+  end 
+end 
